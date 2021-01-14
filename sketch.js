7ba9c935dbfd2c5d1f0970e7f914a2a5ece651ee
hemontakhan <76201341+hemontakhan@users.noreply.github.com>
2021-01-14 20:00:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,79 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var roof;
+var bob1;
+var rope1;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	roof= new Roof(350,100,300,30);
+	World.add(world,roof);
+	
+	bob1=new Bob(200,105,65);
+	bob2=new Bob(210,105,65);
+	bob3=new Bob(220,105,65);
+	bob4=new Bob(230,105,65);
+	bob5=new Bob(240,105,65);
+
+
+	rope1=new Rope(bob1.body,roof.body);
+	World.add(world,rope1);
+
+	rope2=new Rope(bob2.body,roof.body);
+	World.add(world,rope2);
+	
+	rope3=new Rope(bob3.body,roof.body);
+	World.add(world,rope3);
+
+    rope4=new Rope(bob4.body,roof.body);
+	World.add(world,rope4);
+
+	rope5=new Rope(bob5.body,roof.body);
+	World.add(world,rope5);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("grey");
+  
+  Engine.update(engine);
+  
+  bob1.display();
+  bob2.display();
+  bob3.display();
+  bob4.display();
+  bob5.display();
+
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
+
+  roof.display();
+
+  drawSprites();
+ 
+}
+
+
+
